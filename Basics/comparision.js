3a9afeb30c6546ea43fa160d3b7fc84f09069450
raj5125 <140936711+raj5125@users.noreply.github.com>
2023-08-26 18:21:44 +0000

    comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,26 @@
+/* same datatypes comparision not any problem */
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+/* diff. datatypes comparision leads to confusion 
+also here == , === is equality check while <, >= are
+comaparision operators work differently*/
+
+// console.log("2" > 1);     //true as string converted to number and then compared
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === this check datatype also so here string not equal to number
+
+console.log("2" === 2);     //false
