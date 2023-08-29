58e244c782553671b284a43a5b917a5bdb3b7a10
raj5125 <140936711+raj5125@users.noreply.github.com>
2023-08-29 17:36:23 +0000

    control flow

---
+++ Controlflow/switch.js
@@ -0,0 +1,30 @@
+/*
+switch(key)
+{
+    case value:
+        break;
+    case value:
+        break;
+    default:
+        break;
+}
+*/
+
+const month = 4
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feburary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("default case match");
+        break;
+};
