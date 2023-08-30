de05e80d492cab2bf4254c6f689213a97d44e846
raj5125 <140936711+raj5125@users.noreply.github.com>
2023-08-30 11:07:44 +0000

    reduce

---
+++ Loops/reduce.js
@@ -0,0 +1,32 @@
+const arr = [1, 2, 3]
+const myTotal = arr.reduce( function(acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// 0 at end represents accumulator value initially means here initially shopping cart value is zero
+console.log(priceToPay);
