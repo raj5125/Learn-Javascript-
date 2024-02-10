7546aa99999437d03e82adfa597a78476c339c8b
raj5125 <140936711+raj5125@users.noreply.github.com>
2024-02-10 10:10:15 +0000

    comparision

--- Basics/comparision.js
+++ Basics/comparision.js
@@ -13,14 +13,15 @@ comaparision operators work differently*/
 // console.log("2" > 1);     //true as string converted to number and then compared
 // console.log("02" > 1);
 
+// avoid these below comparision as it always makes confusion
 console.log(null > 0);
-console.log(null == 0);
+console.log(null == 0);        
 console.log(null >= 0);
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === this check datatype also so here string not equal to number
+// === this check datatype also so here string not equal to number that's why it is also called strict check
 
 console.log("2" === 2);     //false
